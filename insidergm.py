b0e85f4133a974db16227727f36bddf5a83990cc
tom0102 <91820207+tom0102@users.noreply.github.com>
2021-10-03 15:56:13 +0900

    Update insidergm.py

--- insidergm.py
+++ insidergm.py
@@ -36,7 +36,7 @@ answers = ['りんご','ごりら','ばなな']
 defaultAnswerset = 'default'
 
 # 自分のBotのアクセストークンに置き換えてください
-TOKEN = ''
+TOKEN = 'ODkzODA2OTE3NDA1Nzg2MTIy.YVg0jw.Vf2QuwhikLoZ0lIrkLDsMU_riWA'
 
 # 接続に必要なオブジェクトを生成
 client = commands.Bot(command_prefix='/')
